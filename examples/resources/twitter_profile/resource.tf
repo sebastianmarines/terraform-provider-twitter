8468e1e5a6ffd17752f572fa040bda4b4a7c34c0
Sebastian Marines <sebastian0marines@gmail.com>
2022-07-19 17:23:21 -0500

    Generate documentation for profile resource

---
+++ examples/resources/twitter_profile/resource.tf
@@ -0,0 +1,6 @@
+resource "twitter_profile" "me" {
+  name        = "Rick Astley"
+  url         = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  location    = "Goland"
+  description = "Set from my terraform provider"
+}
