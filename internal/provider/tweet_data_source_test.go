b3abf9ff8a8a8f0dc52effdbf66e2ac9e5dcf99e
Sebastian Marines <sebastian0marines@gmail.com>
2022-08-16 09:34:55 -0500

    Create tests for Tweet data source

---
+++ internal/provider/tweet_data_source_test.go
@@ -0,0 +1,38 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccTweetDataSource(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceConfig(1559537820804399104),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.twitter_tweet.acc", "text", "Hello World!"),
+				),
+			},
+			{
+				Config:      testAccDataSourceConfig(1),
+				ExpectError: regexp.MustCompile("Unable to read tweet, got error: .+"),
+			},
+		},
+	})
+}
+
+func testAccDataSourceConfig(id int64) string {
+	return fmt.Sprintf(`
+data "twitter_tweet" "acc" {
+  id = %[1]q
+}`, strconv.FormatInt(id, 10))
+
+}
